582e30f2c8e4a276a58e7f11b91820e7e5ac1ac4
thulioolimpio <Thulioolimpio25@gmail.com>
2024-07-18 14:27:57 -0300

    ..

---
+++ exercícios/016/ex01.py
@@ -0,0 +1,11 @@
+print('--------------------------------------')
+print('Escola Aplos Olímpio Caetano da Silva')
+print('--------------------------------------')
+
+quantidade = int(input('Bem-vindo ao portal do professor! Quantos alunos você deseja atualizar a média?  '))
+
+pessoa = ""
+
+maior = 0
+
+while 
